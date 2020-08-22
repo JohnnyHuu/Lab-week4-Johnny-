eb27950d677cc6067c1a64ae0d47b954cfd16e01
JohnnyHuu <13431453@student.uts.edu.au>
2020-08-22 19:51:24 +0800

    60 percent complete

--- Assets/Scenes/LoadAssets.cs
+++ Assets/Scenes/LoadAssets.cs
@@ -2,20 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+[System.Serializable]
+public class LoadAssets : MonoBehaviour
 {
+    [SerializeField] public GameObject LoadManager;
+    [SerializeField] public GameObject RedSphere;
+    [SerializeField] public GameObject blueSphere;
+
     // Start is called before the first frame update
+
+
+
+    
     void Start()
     {
-        
+        GameObject.Find("LoadManager");
+       Instantiate(RedSphere, new Vector3(2, 0, 0), new Quaternion(0, 0, 0, 0));
+       Instantiate(blueSphere, new Vector3(-2, 0, 0), new Quaternion(0, 0, 0, 0));
+
     }
 
     // Update is called once per frame
+    
     void Update()
     {
         
     }
-    void redObj()
-    { 
+    
+    
+    public void redObj()
+    {
+     
+    }
+
+
+    private void blueObj()
+    {
+
     }
 }
+
