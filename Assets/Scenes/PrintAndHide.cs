3b8135f2c6dbbb6cd64aaf8cb36ade0d8d684946
JohnnyHuu <13431453@student.uts.edu.au>
2020-08-22 22:09:26 +0800

    100 percent complete

---
+++ Assets/Scenes/PrintAndHide.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int result = 0;
+    
+    public Renderer rend;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Update();
+       
+
+
+    }
+
+
+    //
+    public void Update()
+    {
+        
+        for (int i = 0; i < 133; i++)
+        {
+            int result;
+            result = i;
+           
+                Debug.Log("RedSphere :" + result);
+            Debug.Log("blueSphere :" + result);
+
+
+
+        }
+
+        
+    }
+}
