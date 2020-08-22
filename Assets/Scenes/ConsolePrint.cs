0f25f7d055dc600467633a8556a58792669bf595
JohnnyHuu <13431453@student.uts.edu.au>
2020-08-22 21:48:57 +0800

    80 percent complete

---
+++ Assets/Scenes/ConsolePrint.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int result = 0;
+    public GameObject RedSphere;
+    public GameObject blueSphere;
+    public GameObject GameObject;
+    // Start is called before the first frame update
+    public void Start()
+    {
+        Update();
+       
+
+
+    }
+
+
+    //
+    public void Update()
+    {
+        
+        for (int i = 0; i < 133; i++)
+        {
+            int result;
+            result = i;
+           
+                Debug.Log("RedSphere :" + result);
+            Debug.Log("blueSphere :" + result);
+
+
+
+        }
+
+
+
+    }
+}
